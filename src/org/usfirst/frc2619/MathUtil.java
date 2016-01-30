731a0388e20deedc800d5f534631e6df087081ec
Cole Schaar <schaarCA@gmail.com>
2016-01-30 14:20:34 -0500

    Changed the name of the delinearization method. added the smartdashboard getters and setters for the power and the deadband values

--- src/org/usfirst/frc2619/MathUtil.java
+++ src/org/usfirst/frc2619/MathUtil.java
@@ -2,7 +2,7 @@ package org.usfirst.frc2619;
 
 public class MathUtil {
 	
-	public static double delinerize(double input, int power)
+	public static double delinearize(double input, int power)
 	{		
 		return Math.pow(input, (double) power);
 	}
