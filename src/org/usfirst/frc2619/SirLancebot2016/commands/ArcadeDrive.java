1b2b6c4226e66ffccd5438ae117164d165be5a5f
Cole Schaar <schaarCA@gmail.com>
2016-01-30 15:36:09 -0500

    Created DriveBase and its extensions and sendablechooser

---
+++ src/org/usfirst/frc2619/SirLancebot2016/commands/ArcadeDrive.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc2619.SirLancebot2016.commands;
+
+import org.usfirst.frc2619.SirLancebot2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ArcadeDrive extends DriveBase {
+
+    public ArcadeDrive() {
+    	super();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    protected boolean isFinished() {
+    	boolean enabled = Robot.oi.driveMode.getSelected().equals("arcade");
+    	
+        return !enabled || super.isFinished();
+    }
+
+    protected double getLeft(){
+    	double leftspeed = Robot.oi.leftJoystick.getY();
+    	
+    	leftspeed *= -1;
+    	
+    	return leftspeed;
+    }
+    
+    protected double getRight(){
+    	double rightspeed = Robot.oi.rightJoystick.getY();
+    	
+    	rightspeed *= -1;
+    	
+    	return rightspeed;
+    }
+}
---
+++ src/org/usfirst/frc2619/SirLancebot2016/commands/HaloDrive.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc2619.SirLancebot2016.commands;
+
+import org.usfirst.frc2619.SirLancebot2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class HaloDrive extends DriveBase {
+
+    public HaloDrive() {
+    	super();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    protected boolean isFinished() {
+    	boolean enabled = Robot.oi.driveMode.getSelected().equals("halo");
+    	
+        return !enabled || super.isFinished();
+    }
+
+    protected double getLeft(){
+    	double leftspeed = Robot.oi.leftJoystick.getY();
+    	
+    	leftspeed *= -1;
+    	
+    	return leftspeed;
+    }
+    
+    protected double getRight(){
+    	double rightspeed = Robot.oi.rightJoystick.getY();
+    	
+    	rightspeed *= -1;
+    	
+    	return rightspeed;
+    }
+}
