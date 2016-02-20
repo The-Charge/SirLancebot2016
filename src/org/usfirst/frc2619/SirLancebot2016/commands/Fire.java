5ecd684553280657ad18ec7702718db67d25c18d
Richard Wall <wallr506@gmail.com>
2016-02-20 12:36:24 -0500

    Added code to Fire. It Bops and after a second retracts the plunger. Deleted the Bop command

--- src/org/usfirst/frc2619/SirLancebot2016/commands/Fire.java
+++ src/org/usfirst/frc2619/SirLancebot2016/commands/Fire.java
@@ -38,23 +38,30 @@ public class Fire extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	setTimeout(1);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.plunger.bop();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if(isTimedOut())
+    		return true;
+    	else
+    		return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.plunger.retract();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
