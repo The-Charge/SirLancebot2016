d07cdb401b9bdfd36ac0694909b6daf7f3e68eac
Sparky-1 <richardw@hesonline.com>
2016-03-12 16:53:45 -0500

    ShooterMotorsSpit was returning false, now returns true

--- src/org/usfirst/frc2619/SirLancebot2016/commands/ShooterMotorsSpit.java
+++ src/org/usfirst/frc2619/SirLancebot2016/commands/ShooterMotorsSpit.java
@@ -49,7 +49,7 @@ public class ShooterMotorsSpit extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
