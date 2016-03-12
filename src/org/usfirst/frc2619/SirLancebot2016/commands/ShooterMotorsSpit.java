ac4bf8b6fe948d42c6ddef2dc3e0ca605be7660c
AmandaKline <mandyhappypeace@gmail.com>
2016-03-12 13:32:37 -0500

    ShooterMotorsSpit() was not coded, it is now

--- src/org/usfirst/frc2619/SirLancebot2016/commands/ShooterMotorsSpit.java
+++ src/org/usfirst/frc2619/SirLancebot2016/commands/ShooterMotorsSpit.java
@@ -42,6 +42,9 @@ public class ShooterMotorsSpit extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.shooterMotors.writeDashboardDebugValues();
+    	Robot.shooterMotors.shooterMotorsOn = true;
+    	Robot.shooterMotors.shooterMotorsForward = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
