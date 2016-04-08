85d74e43662c387720344cf36454452f54fe08e2
abcurrie <abcurrie@gmail.com>
2016-04-08 13:47:44 -0400

    added values to SmartDashboard

--- src/org/usfirst/frc2619/TheChargeDashboard.java
+++ src/org/usfirst/frc2619/TheChargeDashboard.java
@@ -25,7 +25,7 @@ public class TheChargeDashboard {
 			"onDistanceTarget",
 			"OptimalDistance",
 			"DistanceDeadband",
-			/* Blinky Lights */ "LightSpike", "LEDState", "BallSensor", "Hasball", "hasBallBool",
+			/* Blinky Lights */ "LightSpike", "LEDState", "BallSensor", "Hasball", "hasBallBool", "ShooterUp",
 			/* GyroSubsystem */"IMU_TotalYaw",
 			"IMU_Roll",
 			"IMU_Pitch",
