f6a1a7a16742f3eaa099508fdcabe212a0776169
abcurrie <abcurrie@gmail.com>
2016-04-24 12:53:55 -0400

    minor changes to tuning

--- src/org/usfirst/frc2619/SirLancebot2016/subsystems/DriveTrain.java
+++ src/org/usfirst/frc2619/SirLancebot2016/subsystems/DriveTrain.java
@@ -62,10 +62,10 @@ public class DriveTrain extends Subsystem {
 	private final double DEFAULT_DEADBANDZ = .15;
 	private final double DEFAULT_DELIN_POWER = 3;
 
-	private final static double DEFAULT_AIM_DISTANCE_SPEED = .2;
+	private final static double DEFAULT_AIM_DISTANCE_SPEED = .1;
 	private final static double DEFAULT_AIM_ANGLE_SPEED = .15;
-	private final static double DISTANCE_GAIN = .075;
-	private final static double ANGLE_GAIN = .025;
+	private final static double DISTANCE_GAIN = .01;
+	private final static double ANGLE_GAIN = .01;
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
